316ee4758e2248e7c0910f86e71afff442241617
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-03-05 11:25:58 +0400

    Create of Main class and add menu Items

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main {
+	private ArrayList<String> menuItems;
+
+	public Main() {
+		menuItems = new ArrayList<String>();
+		menuItems.add("Shop Settings");
+		menuItems.add("Manage Shop Items");
+		menuItems.add("Create New Invoice");
+		menuItems.add("Report: Statistics");
+		menuItems.add("Report: All Invoices");
+		menuItems.add("Search Invoice");
+		menuItems.add("Program Statistics");
+		menuItems.add("Exit");
+	}
+
+}
