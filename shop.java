516af0dbc3919d7a5faa9fb5692cd66497a9391b
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-03-06 11:48:39 +0400

    Create shop class with getter and setter

---
+++ shop.java
@@ -0,0 +1,20 @@
+public class shop {
+	
+	private String shopName;
+	private int shopID;
+	
+	
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public int getShopID() {
+		return shopID;
+	}
+	public void setShopID(int shopID) {
+		this.shopID = shopID;
+	}
+
+}
