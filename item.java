8d05864840236b8db801abbe166a89df6a6c3413
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-03-06 10:38:27 +0400

    Create item class with getter and setter

---
+++ item.java
@@ -0,0 +1,47 @@
+public class item {
+
+	private String itemID;
+	private String itemName;
+	private double unitPrice;
+	private int quantity;
+	private double qtyAmount;
+	
+	
+	
+	public String getItemID() {
+		return itemID;
+	}
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getQtyAmount() {
+		return qtyAmount;
+	}
+	public void setQtyAmount(double qtyAmount) {
+		this.qtyAmount = qtyAmount;
+	}
+
+	
+	
+	
+	
+	
+}
