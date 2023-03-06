a64074813b8a360838794a1ee95379f22e0c9832
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-03-06 10:05:56 +0400

    Create customer class with getter and setter

---
+++ customer.java
@@ -0,0 +1,37 @@
+public class customer {
+	private int customerId;
+	private String customerFirstName;
+	private String customerSecondName;
+	private int customerPhoneNumber;
+	
+	
+	
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+	public String getCustomerSecondName() {
+		return customerSecondName;
+	}
+	public void setCustomerSecondName(String customerSecondName) {
+		this.customerSecondName = customerSecondName;
+	}
+	public int getCustomerPhoneNumber() {
+		return customerPhoneNumber;
+	}
+	public void setCustomerPhoneNumber(int customerPhoneNumber) {
+		this.customerPhoneNumber = customerPhoneNumber;
+	}
+	
+	
+	
+}
